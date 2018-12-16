4722d0dc8b92e55c8a16a642c124e925104b45c8
Dung Le Anh <dungla4@fsoft.com.vn>
2018-12-16 17:33:35 +0700

    Add config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': ['error', { 'allow': ['_id'] }],
   },
   parserOptions: {
     parser: 'babel-eslint',
