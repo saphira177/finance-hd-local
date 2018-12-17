d76dc3866810888876237d69f56f789174e751d5
Dung Le Anh <dungla4@fsoft.com.vn>
2018-12-17 19:40:10 +0700

    Implement invoice module

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import VuexPersistence from 'vuex-persist';
 import users from './modules/users';
 import groups from './modules/groups';
+import invoices from './modules/invoices';
 
 Vue.use(Vuex);
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
   modules: {
     users,
     groups,
+    invoices,
   },
   plugins: [
     new VuexPersistence({ storage: window.localStorage }).plugin,
---
+++ tests/unit/store/modules/groups.spec.js
@@ -0,0 +1,95 @@
+import { findIndex, map } from 'lodash';
+import {
+  actions,
+  getters,
+  mutations,
+} from '@/store/modules/groups';
+
+describe('groups module', () => {
+  let state;
+
+  beforeEach(() => {
+    state = {
+      groups: [
+        { _id: 'group1', name: 'Rent', total: 5000 },
+        { _id: 'group2', name: 'Monthly', total: 18000 },
+      ],
+    };
+  });
+
+  describe('mutations', () => {
+    describe('add', () => {
+      it('should add new item to groups', () => {
+        mutations.add(state, { _id: 'newGroup' });
+        expect(state.groups).toHaveLength(3);
+      });
+    });
+
+    describe('update', () => {
+      it('should update existing item', () => {
+        mutations.update(state, { _id: 'group2', name: 'Updated group 2' });
+        expect(state.groups[1]).toEqual({
+          _id: 'group2', name: 'Updated group 2', total: 18000,
+        });
+      });
+
+      it('should not update if non-existing item', () => {
+        mutations.update(state, { _id: 'group0', name: 'Updated group 0' });
+        expect(findIndex(state.groups, { name: 'Updated group 0' })).toEqual(-1);
+      });
+
+      it('should not update if missing _id field', () => {
+        mutations.update(state, { name: 'Updated group 0' });
+        expect(findIndex(state.groups, { name: 'Updated group 0' })).toEqual(-1);
+      });
+    });
+
+    describe('remove', () => {
+      it('should remove group by id', () => {
+        mutations.remove(state, 'group1');
+        expect(state.groups).toHaveLength(1);
+        expect(map(state.groups, '_id')).toEqual(['group2']);
+      });
+    });
+  });
+
+  describe('actions', () => {
+    let commit;
+    beforeEach(() => {
+      commit = jest.fn();
+    });
+
+    describe('addGroup', () => {
+      it('should commit add event', () => {
+        actions.addGroup({ commit }, { _id: 'newGroup' });
+        expect(commit).toHaveBeenCalledWith('add', { _id: 'newGroup' });
+      });
+    });
+
+    describe('removeGroup', () => {
+      it('should commit add event', () => {
+        actions.removeGroup({ commit }, 'groupId');
+        expect(commit).toHaveBeenCalledWith('remove', 'groupId');
+      });
+    });
+  });
+
+  describe('getters', () => {
+    describe('allGroups', () => {
+      it('should list all groups', () => {
+        expect(getters.allGroups(state)).toEqual([
+          { _id: 'group1', name: 'Rent', total: 5000 },
+          { _id: 'group2', name: 'Monthly', total: 18000 },
+        ]);
+      });
+    });
+
+    describe('group', () => {
+      it('should return group by id', () => {
+        expect(getters.group(state)('group2')).toEqual(
+          { _id: 'group2', name: 'Monthly', total: 18000 },
+        );
+      });
+    });
+  });
+});
