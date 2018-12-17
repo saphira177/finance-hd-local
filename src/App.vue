dfeaaff6344662b63921f475a7e92b92f567e080
Dung Le Anh <dungla4@fsoft.com.vn>
2018-12-17 20:10:44 +0700

    Implement sidebar and router

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,70 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+    <v-toolbar dark color="primary" app>
+      <v-toolbar-side-icon @click="toggleMenu"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">{{title}}</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-title class="white--text">FINANCE HD</v-toolbar-title>
     </v-toolbar>
+    <v-navigation-drawer v-model="open" app>
+      <v-list dense class="pt-3">
+        <v-list-tile
+          v-for="item in menu"
+          :key="item.title"
+          :to="item.to"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-fade-transition mode="out-in">
+          <router-view></router-view>
+        </v-fade-transition>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
   components: {
-    HelloWorld,
   },
   data() {
     return {
-      //
+      menu: [
+        { title: 'Home', icon: 'home', to: '/' },
+        { title: 'About', icon: 'spa', to: '/about' },
+      ],
+      open: false,
     };
+  },
+  computed: {
+    title() {
+      const active = this.menu.filter(item => item.to === this.$route.path);
+      let title = '';
+      if (active.length > 0) {
+        [{ title }] = active;
+      }
+      return title;
+    },
   },
   beforeCreate() {
     // App before
+  },
+  methods: {
+    toggleMenu() {
+      this.open = !this.open;
+    },
   },
 };
 </script>
