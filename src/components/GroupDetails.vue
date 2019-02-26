21791004caeaabb7abf97445ff8cec91dc36d695
Dung Le Anh <anhdungle1986@gmail.com>
2019-02-26 15:03:30 +0700

    Show list invoices in group details page

--- src/components/GroupDetails.vue
+++ src/components/GroupDetails.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="group-details">
-    <p class="title">{{item.name}} <span
-      :class="['headline', item.available >= 0 ? 'blue--text' : 'red--text']"
-    >{{toCurrency(item.available)}}</span></p>
+    <p class="title">{{group.name}} <span
+      :class="['headline', group.available >= 0 ? 'blue--text' : 'red--text']"
+    >{{toCurrency(group.available)}}</span></p>
     <v-list>
       <v-list-tile
         v-for="category in categoriesAmount"
@@ -18,19 +18,28 @@
         </v-list-tile-action>
       </v-list-tile>
     </v-list>
+    <p class="headline mt-5">List invoices</p>
+    <invoice-table :invoices="invoicesByGroup(group._id)" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Getter } from 'vuex-class';
 import { displayMoney } from '@/utils/decorators';
+import InvoiceTable from './InvoiceTable.vue';
 
-@Component
+@Component({
+  components: {
+    InvoiceTable,
+  },
+})
 export default class GroupDetails extends Vue {
   @Prop({
     default: () => ({ _id: '', name: '', available: -1 }),
-  }) public item!: Group;
+  }) public group!: Group;
   @Prop() public categoriesAmount!: Array<CategoryAmount>;
+  @Getter invoicesByGroup!: any;
 
   public toCurrency(amount: number): string {
     return displayMoney(amount);
